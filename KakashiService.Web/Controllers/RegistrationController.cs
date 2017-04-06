8229657e02f8d5e82489307072819ae83f133505
Luan Caius <luancaius@outlook.com>
2017-04-05 23:34:19 -0300

    Starting to using web project since unit test is not getting the appsettings correctly.

---
+++ KakashiService.Web/Controllers/RegistrationController.cs
@@ -0,0 +1,26 @@
+﻿using KakashiService.Core.Entities;
+using System.Configuration;
+using System.Web.Mvc;
+
+namespace KakashiService.Web.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var serviceObject = new ServiceObject();
+            serviceObject.IISPath = ConfigurationManager.AppSettings["iisPath"];
+            serviceObject.MsBuildPath = ConfigurationManager.AppSettings["msbuildPath"];
+            serviceObject.SvcUtilPath = ConfigurationManager.AppSettings["svcutilPath"];
+            return View();
+        }
+
+        public JsonResult RegisterEndPoint(string endpoint)
+        {
+
+
+            return Json(new object { });
+        }
+    }
+}
