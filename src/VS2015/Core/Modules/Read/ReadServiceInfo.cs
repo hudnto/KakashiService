2c55ed31be5eb70e0d7661d64e6121f5c50bda67
Luan Caius <luancaius@outlook.com>
2017-07-24 21:51:02 -0300

    Revert "Deleting VS2015 from VS2012 branch"
    This reverts commit 3a863ce233b00658fdf04ab8dd9985ef7b1c23d0.

---
+++ src/VS2015/Core/Modules/Read/ReadServiceInfo.cs
@@ -0,0 +1,215 @@
+﻿using KakashiService.Core.Entities;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web.Services.Description;
+using System.Xml.Schema;
+
+namespace KakashiService.Core.Modules.Read
+{
+    public class ReadServiceInfo
+    {
+        private List<ObjectType> _objectTypes;
+
+        public ReadServiceInfo()
+        {
+            _objectTypes = new List<ObjectType>();
+        }
+
+        private void AddObject(ObjectType objectType)
+        {
+            _objectTypes.Add(objectType);
+        }
+
+        public List<ObjectType> GetObjectTypes()
+        {
+            return _objectTypes;
+        }
+
+        public ServiceDescription GetServiceDescriptionFromSVC(string url)
+        {
+            UriBuilder uriBuilder = new UriBuilder(url);
+            uriBuilder.Query = "WSDL";
+
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(uriBuilder.Uri);
+            webRequest.ContentType = "text/xml;charset=\"utf-8\"";
+            webRequest.Method = "GET";
+            webRequest.Accept = "text/xml";
+
+            ServiceDescription serviceDescription;
+            try
+            {
+                using (WebResponse response = webRequest.GetResponse())
+                {
+                    using (Stream stream = response.GetResponseStream())
+                    {
+                        serviceDescription = ServiceDescription.Read(stream);
+                    }
+                }
+            }
+            catch (WebException e)
+            {
+                throw new WebException("Error while reading service. Check if it is online.", e);
+            }
+
+            return serviceDescription;
+        }
+
+        public XmlSchemaSet GetAllSchema(ServiceDescription serviceDescription)
+        {
+            var schemaSet = new XmlSchemaSet();
+            foreach (XmlSchema schema in serviceDescription.Types.Schemas)
+            {
+                if (schema.Includes.Count == 0)
+                {
+                    schemaSet.Add(schema);
+                }
+                else
+                {
+                    foreach (var include in schema.Includes)
+                    {
+                        var schemaLocation = ((XmlSchemaImport)include).SchemaLocation;
+                        schemaSet.Add(schema.SourceUri, schemaLocation);
+                    }
+                }
+            }
+
+            return schemaSet;
+        }
+
+        public List<Functions> ExtractItemXml(XmlSchemaSet schemaSet)
+        {
+            var functionsResponse = new List<Functions>();
+            var functions = new List<Functions>();
+            foreach (XmlSchema xmlSchema in schemaSet.Schemas())
+            {
+                //TODO Make a better solution
+                if (xmlSchema.TargetNamespace.Contains("Serialization"))
+                    continue;
+                foreach (object item in xmlSchema.Items)
+                {
+                    var function = new Functions();
+                    var functionResponse = new Functions();
+                    XmlSchemaElement schemaElement = item as XmlSchemaElement;
+                    XmlSchemaComplexType complexType = item as XmlSchemaComplexType;
+                    var index = 0;
+
+                    if (schemaElement != null)
+                    {
+                        if (schemaElement.Name.Contains("Response"))
+                        {
+                            functionResponse.Name = schemaElement.Name;
+                        }
+                        else
+                        {
+                            function.Name = schemaElement.Name;
+                        }
+                        XmlSchemaType schemaType = schemaElement.SchemaType;
+                        XmlSchemaComplexType schemaComplexType = schemaType as XmlSchemaComplexType;
+
+                        if (schemaComplexType != null)
+                        {
+                            XmlSchemaParticle particle = schemaComplexType.Particle;
+                            XmlSchemaSequence sequence = particle as XmlSchemaSequence;
+
+                            if (sequence != null)
+                            {
+                                foreach (XmlSchemaElement childElement in sequence.Items)
+                                {
+                                    if (schemaElement.Name.Contains("Response"))
+                                    {
+                                        functionResponse.ReturnType = childElement.SchemaTypeName.Name;
+                                    }
+                                    else
+                                    {
+                                        function.Parameters.Add(new Parameter(index, childElement.SchemaTypeName.Name));
+                                        index++;
+                                    }
+                                }
+                            }
+                            if (schemaComplexType.AttributeUses.Count > 0)
+                            {
+                                IDictionaryEnumerator enumerator = schemaComplexType.AttributeUses.GetEnumerator();
+
+                                while (enumerator.MoveNext())
+                                {
+                                    XmlSchemaAttribute attribute = (XmlSchemaAttribute)enumerator.Value;
+
+                                    Console.Out.WriteLine("      Attribute/Type: {0}", attribute.Name);
+                                }
+                            }
+                            if (schemaElement.Name.Contains("Response"))
+                            {
+                                functionsResponse.Add(functionResponse);
+                            }
+                            else
+                            {
+                                functions.Add(function);
+                            }
+                        }
+
+                    }
+                    else if (complexType != null)
+                    {
+                        Console.Out.WriteLine("Complex Type: {0}", complexType.Name);
+                        var objectType = new ObjectType(index, complexType.Name);
+                        OutputElements(complexType.Particle, objectType);
+                        if (complexType.AttributeUses.Count > 0)
+                        {
+                            IDictionaryEnumerator enumerator = complexType.AttributeUses.GetEnumerator();
+
+                            while (enumerator.MoveNext())
+                            {
+                                XmlSchemaAttribute attribute = (XmlSchemaAttribute)enumerator.Value;
+                                Console.Out.WriteLine("      Attribute/Type: {0}", attribute.Name);
+                            }
+                        }
+                    }
+                }
+            }
+
+            foreach (var response in functionsResponse)
+            {
+                var functionName = response.Name.Replace("Response", "");
+                var function = functions.First(a => a.Name == functionName);
+                function.ReturnType = response.ReturnType;
+            }
+
+
+            return functions;
+        }
+
+        private void OutputElements(XmlSchemaParticle particle, ObjectType objectType)
+        {
+            XmlSchemaSequence sequence = particle as XmlSchemaSequence;
+            XmlSchemaChoice choice = particle as XmlSchemaChoice;
+            XmlSchemaAll all = particle as XmlSchemaAll;
+            if (sequence != null)
+            {
+                Console.Out.WriteLine("  Sequence");
+
+                for (int i = 0; i < sequence.Items.Count; i++)
+                {
+                    XmlSchemaElement childElement = sequence.Items[i] as XmlSchemaElement;
+                    XmlSchemaComplexType childComplexType = sequence.Items[i] as XmlSchemaComplexType;
+
+                    if (childElement != null)
+                    {
+                        Console.Out.WriteLine("    Element/Type: {0}:{1}", childElement.Name,
+                            childElement.SchemaTypeName.Name);
+                        objectType.Attributes.Add(childElement.SchemaTypeName.Name);
+                    }
+                    else
+                    {
+                        var name = childComplexType.Name;
+                        OutputElements(sequence.Items[i] as XmlSchemaParticle, new ObjectType(0, name));
+                    }
+                }
+                AddObject(objectType);
+            }
+        }
+    }
+}
