2146d1bac21018e07bb14c4a3e3978d7338b3ce7
Luan Caius <luancaius@outlook.com>
2017-07-28 22:02:04 -0300

    Feature: adding new controller to read services and show results.

---
+++ src/VS2015/UI/Controllers/ReadServiceController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KakashiService.Core.Entities;
+using KakashiService.Core.Services;
+using KakashiService.Web.ViewModel;
+
+namespace KakashiService.Web.Controllers
+{
+    public class ReadServiceController : Controller
+    {
+        // GET: ReadService
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Read(String Url)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Json(new { success = false });
+            }
+            var readService = new ReadService();
+            var serviceObject = new ServiceObject();
+            try
+            {
+                readService.Execute(serviceObject);
+                return Json(new { success = true, modal = new {  title = "Operation Completed!" } }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception e)
+            {             
+                return Json(new { success = false, modal = new { title = "Operation Fail!" } }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
---
+++ src/VS2015/UI/Scripts/ReadService/Index.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    $('#btnClone').click(SubmitForm);
+})();
+
+function SubmitForm() {
+    $('#result div').remove();
+    var urlData = $('#Url').val();
+    var json = { Url: urlData };
+    var url = urlRoot + '/ReadService/Read';
+    $.ajax({
+        data: json,     
+        method: 'POST',
+        url: url,
+        success: function (data) {
+            PrintResult(data);
+        }
+    }).fail(function (data) {
+        console.log(data);
+    });
+}
+
+function PrintResult(success, message) {
+
+}
