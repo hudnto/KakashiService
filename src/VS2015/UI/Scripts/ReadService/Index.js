fe013a0e7bb7d509221cc0438a2cc567a4dd2adc
Luan Caius <luancaius@outlook.com>
2017-08-02 22:34:15 -0300

    Result of the read service written on the page

--- src/VS2015/UI/Scripts/ReadService/Index.js
+++ src/VS2015/UI/Scripts/ReadService/Index.js
@@ -7,6 +7,7 @@ function SubmitForm() {
     var urlData = $('#Url').val();
     var json = { Url: urlData };
     var url = urlRoot + '/ReadService/Read';
+    ClearResult();
     $.ajax({
         data: json,     
         method: 'POST',
@@ -23,5 +24,21 @@ function PrintResult(data) {
     var functions = data.response.functions;
     var total = data.response.totalFunctions;
     var name = data.response.name;
-    var totalObject = data.response.totalObject;            
+    var totalObject = data.response.totalObject;
+
+    $('#response').append('<div></div>');
+
+    var $response = $('#response div');
+    $response.append("<label class='control-label'>Service Name: " + name);
+
+    $response.append("<label class='control-label'>Total functions: " + total);
+    functions.forEach(function(item, index) {
+        $response.append("<label class='block'>" + (index + 1) + " - " + item);
+    });
+
+    $response.append("<label class='control-label'>Total objects: " + totalObject);
+}
+
+function ClearResult() {
+    $('#response div').remove();
 }
