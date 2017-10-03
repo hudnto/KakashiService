dcc42d6cd0f3d224a8c343d2be886e04a2977f31
Luan Caius Ramos <luancaius@outlook.com>
2017-10-03 14:59:25 -0300

    Adding unit test project

---
+++ src/VS2015/KakashiService.UnitTest/Services/Modules/ReadModule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KakashiService.UnitTest
+{
+    [TestClass]
+    public class ReadModule
+    {
+        [TestMethod]
+        public void ReadXML1AndChecks()
+        {
+
+        }
+    }
+}
